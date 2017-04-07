5b5f9abebff3b2fc78d4cf6f589d359a3be432af
David Kogan <david.kogan@interrogare.de>
2017-04-07 14:10:14 +0200

    Lib hinzugefügt, nicht benötigte npm module entfernt

--- src/styleguide/helpers/helper-templates-nav.js
+++ src/styleguide/helpers/helper-templates-nav.js
@@ -1,5 +1,5 @@
 (function () {
-  var _sgu = require('styleguide-utils');
+  var _sgu = require('../../../lib/js/styleguide-utils');
   module.exports.register = function (Handlebars, options) {
     Handlebars.registerHelper("templates-nav", function (src) {
       return new Handlebars.SafeString(_sgu.generatedStyleGuideNavBar(src));
